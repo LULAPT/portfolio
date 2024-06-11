bf0b0d5e3e9ce6e4f183be6452943f2c15497d5c
dharlan <90869420+LULAPT@users.noreply.github.com>
2024-06-10 23:54:23 -0300

    23:54

---
+++ script.js
@@ -0,0 +1,7 @@
+/* =========================== escrevendo animação =========================== */
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer",],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
